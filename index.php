5339fc67268df5a73245540b2923f09c6e88d9a6
sowil6 <sowil6@gmail.com>
2021-11-15 11:16:42 -0500

    aumentar porcentage de html

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php 
 session_start();
- define("RUTA_BASE", dirname(realpath(__FILE__))."/");
-//echo "en index.php " .RUTA_BASE;
+define("RUTA_BASE", dirname(realpath(__FILE__))."/");
 include "libreria/core.php";
-//echo "en index la ruta de model esss" .RUTA_MODELO."manager";
-/*	 
-
-	if (!isset($_SESSION['username'])) {
-		$_SESSION['msg'] = "You must log in first";
-		header('location: login');
-	}
-if (isset($_GET['logout'])) {
-		
-		session_destroy();
-		unset($_SESSION['username']);
-		header("location: /");
-	}/**/
- 
 ?>
