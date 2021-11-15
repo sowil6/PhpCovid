b3451584f6ce53e3b3187b7678029e03aad88f9f
sowil6 <sowil6@gmail.com>
2021-11-15 12:00:33 -0500

    rutas.oho modificado

--- app/rutas/rutas.php
+++ app/rutas/rutas.php
@@ -6,8 +6,22 @@ $ruta->controladores(
 	array("/" =>"WelcomeController",
 	""=>"InicioController" ,
 	"/institucional"=>"InstitucionalController",
-	""=>"RaizController"
-	
+	"/oferta"=>"OfertaController",
+	"/contacto"=>"ContactoController",
+	"/noticia"=>"AppController",
+	"/catalogoform"=>"CatalogoController",
+	"/proyectos"=>"ProyectoController",
+	"/detallecatalogo"=>"CatalogoDetalleController",
+	"/certificaciones"=>"CertificacionesController",
+	"/estudiante"=>"EstudianteController",
+	"/login"=>"LoginController",
+	"/registro"=>"RegistroController",
+	"/logeado"=>"LogeadoController",
+	"/sin_acceso"=>"SinAccesoController",
+	"/inscripcion"=>"InscripcionController",
+	"/consultarinscripcion"=>"ConsultaInscripcionController",
+	"reporte_inscripcion"=>"Reporte_InscripcionController",
+	"pruebas"=>"PruebasController",
+	" "=>"RaizController"
 	));
-	
 	
